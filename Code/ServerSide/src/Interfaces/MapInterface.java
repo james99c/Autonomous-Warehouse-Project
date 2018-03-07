a6dc070b3ad8f9370e2288e21295f65a42155bac
axj714 <axj714@cs.bham.ac.uk>
2018-03-07 12:35:42 +0000

    Added  extra methods to map interface

--- Code/ServerSide/src/Interfaces/MapInterface.java
+++ Code/ServerSide/src/Interfaces/MapInterface.java
@@ -10,4 +10,21 @@ public interface MapInterface {
 	public Integer getNumberOfRobots();
 	public ArrayList<Pair<Integer, Location>> getRobotsAndIDs();
 	public Location getRobotLocation(Integer robotID);
+	
+	// make this route unaccessible by other robots
+	public void blockRoute(ArrayList<Location> location, Integer RouteID);
+	
+	// make this route available by other robots
+	public void unBlockRouteByID(Integer routeID);
+	
+	public void updateRobotLocation(Integer RobotID, Location newLocation);
+	
+	// returns the robots current route
+	public ArrayList<Location> getRobotsRoute(Integer RobotID);
+	
+	// returns a list of surrounding locations which the robot can move to from that location
+	public ArrayList<Location> getSurroundingLocations(Location location);
+	
+	// returns the heuristic cost from one location to the other
+	public Integer getHeuristicCost(Location startLocation, Location goalLocation);
 }
