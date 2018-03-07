b998ee4555041e2f0ac598506d339a1579d6c18c
jdw725 <JDW725@student.bham.ac.uk>
2018-03-07 12:59:29 +0000

    implemented jobAssigner Interface

--- Code/ServerSide/src/Interfaces/JobAssignerInterface.java
+++ Code/ServerSide/src/Interfaces/JobAssignerInterface.java
@@ -1,5 +1,12 @@
 package Interfaces;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import DataObjects.Job;
+
 public interface JobAssignerInterface {
 
+	public ArrayList<Job> sortJobs(ArrayList<Job> Unsortedjobs);
+	public HashMap<String, ArrayList<Job>> outputJobs(ArrayList<Job> sortedJobs);
 }
