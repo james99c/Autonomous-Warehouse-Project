591e178fc2f867187a3359b5c340d665ece619ff
Henry Fung <hlf764@cs.bham.ac.uk>
2018-03-07 12:34:44 +0000

    Added constructor.

--- Code/ServerSide/src/RoutePlanner.java
+++ Code/ServerSide/src/RoutePlanner.java
@@ -3,5 +3,13 @@ import Interfaces.RoutePlannerInterface;
 
 
 public class RoutePlanner implements RoutePlannerInterface{
+    private ArrayList<Integer> jobs;
 
+    public RoutePlanner() {
+        jobs = getJob();
+    }
+
+    public ArrayList<Integer> getJob() {
+        // Take jobs from JobDecider
+    }
 }
