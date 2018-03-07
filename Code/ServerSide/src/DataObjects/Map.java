6913fcfe965043da5b7f631476775b24990f03e3
axj714 <axj714@cs.bham.ac.uk>
2018-03-07 12:53:12 +0000

    asdf

--- Code/ServerSide/src/DataObjects/Map.java
+++ Code/ServerSide/src/DataObjects/Map.java
@@ -1,5 +1,1 @@
-package DataObjects;
 
-public class Map {
-
-}
---
+++ Code/ServerSide/src/Interfaces/LocationInterface.java
@@ -0,0 +1,8 @@
+package Interfaces;
+
+public interface LocationInterface {
+	public int getX();
+	public int getY();
+	public int[] getCoordinates();
+	public boolean available();
+}
