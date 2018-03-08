edbae4300cc13680f7ac5f14b4c61d2d590a12f2
Henry Fung <hlf764@cs.bham.ac.uk>
2018-03-08 15:14:32 +0000

    getRoute()

--- Code/ServerSide/src/RoutePlanner.java
+++ Code/ServerSide/src/RoutePlanner.java
@@ -5,11 +5,19 @@ import Interfaces.RoutePlannerInterface;
 public class RoutePlanner implements RoutePlannerInterface{
     private ArrayList<Integer> jobs;
 
+    private ArrayList<Location> route;
+
     public RoutePlanner() {
         jobs = getJob();
+    }
+
+    public ArrayList<Location> getRoute() {
+        return this.route;
     }
 
     public ArrayList<Integer> getJob() {
         // Take jobs from JobDecider
     }
+
+    //public search
 }
