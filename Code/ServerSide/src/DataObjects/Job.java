7421c1d9a6ed0e3530446cf0ff67de7f87dd9836
jdw725 <JDW725@student.bham.ac.uk>
2018-03-08 13:33:39 +0000

    Added comparator to sort jobs based on reward value.

--- Code/ServerSide/src/DataObjects/Job.java
+++ Code/ServerSide/src/DataObjects/Job.java
@@ -6,4 +6,24 @@ public class Job implements JobInterface{
 	public Job(){
 		
 	}
+	@Override
+	public Location getStartingLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Location getGoalLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public float getRewardForJob() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public float getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
--- Code/ServerSide/src/JobAssigner.java
+++ Code/ServerSide/src/JobAssigner.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 
 import DataObjects.Job;
@@ -15,7 +17,13 @@ public class JobAssigner implements JobAssignerInterface{
 
 	@Override
 	public ArrayList<Job> sortJobs(ArrayList<Job> Unsortedjobs) {
-		// TODO Auto-generated method stub
+		Collections.sort(jobs, new Comparator<Job>(){
+		     public int compare(Job o1, Job o2){
+		         if(o1.getRewardForJob() == o2.getRewardForJob())
+		             return 0;
+		         return o1.getRewardForJob() > o2.getRewardForJob() ? -1 : 1;
+		     }
+		});
 		return null;
 	}
 
