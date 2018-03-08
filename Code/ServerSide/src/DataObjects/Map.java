8191eea7f39ea128c43278786dcbc30e7d167886
Henry Fung <hlf764@cs.bham.ac.uk>
2018-03-08 15:13:21 +0000

    Exceptions caught.

--- Code/ServerSide/src/DataObjects/Map.java
+++ Code/ServerSide/src/DataObjects/Map.java
@@ -50,13 +50,17 @@ public class Map {
 
 	
 	public ArrayList<GridPoint> getAvailableLocations(Location location, Integer[] timeFrame){
-		ArrayList<GridPoint> surroundingLocations = new  ArrayList<GridPoint>();
-		surroundingLocations.add(map[location.getX() + 1][location.getY()]);
-		surroundingLocations.add(map[location.getX() - 1][location.getY()]);
-		surroundingLocations.add(map[location.getX() ][location.getY() + 1]);
-		surroundingLocations.add(map[location.getX() ][location.getY() - 1]);
+		ArrayList<GridPoint> surroundingLocations = new ArrayList<GridPoint>();
+		try {
+			surroundingLocations.add(map[location.getX() + 1][location.getY()]);
+			surroundingLocations.add(map[location.getX() - 1][location.getY()]);
+			surroundingLocations.add(map[location.getX() ][location.getY() + 1]);
+			surroundingLocations.add(map[location.getX() ][location.getY() - 1]);
+		} catch (IndexOutOfBoundsException e) {
+			// Do nothing
+		}
 		
-		ArrayList<GridPoint> output = new  ArrayList<GridPoint>();
+		ArrayList<GridPoint> output = new ArrayList<GridPoint>();
 		for (GridPoint a: surroundingLocations){
 			if(a.getAvailability(timeFrame)){
 				output.add(a);
