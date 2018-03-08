e882e38ee79dc259817de3d5bfb7301b93b6ab95
jdw725 <JDW725@student.bham.ac.uk>
2018-03-08 13:14:26 +0000

    Implemented empty interface methods.

--- Code/ServerSide/src/JobAssigner.java
+++ Code/ServerSide/src/JobAssigner.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import DataObjects.Job;
 import Interfaces.JobAssignerInterface;
 
 
 public class JobAssigner implements JobAssignerInterface{
+
+	@Override
+	public ArrayList<Job> sortJobs(ArrayList<Job> Unsortedjobs) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<String, ArrayList<Job>> outputJobs(ArrayList<Job> sortedJobs) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
