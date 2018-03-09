5e2e653faa21a627b855215411bc56ba2ca9c73e
onyxement <35332573+onyxement@users.noreply.github.com>
2018-03-09 12:51:45 +0000

    modified values and number of objects in junit test class

--- Code/ServerSide/src/JobAssignerTest.java
+++ Code/ServerSide/src/JobAssignerTest.java
@@ -11,16 +11,22 @@ public class JobAssignerTest {
 
 	@Test
 	public void test() {
-		Job job1 = new Job(5);
-		Job job2 = new  Job(10);
-		Job job3 = new Job(15);
+		Job job1 = new Job((float) 5.0);
+		Job job2 = new Job((float) 10.0);
+		Job job3 = new Job((float) 15.0);
+		Job job4 = new Job((float) 5.1);
+		Job job5 = new Job((float) 100.0);
 		ArrayList<Job> unsortedJobs = new ArrayList<Job>();
 		unsortedJobs.add(job1);
 		unsortedJobs.add(job2);
 		unsortedJobs.add(job3);
+		unsortedJobs.add(job4);
+		unsortedJobs.add(job5);
 		ArrayList<Job> sortedJobs = new ArrayList<Job>();
+		sortedJobs.add(job5);
 		sortedJobs.add(job3);
 		sortedJobs.add(job2);
+		sortedJobs.add(job4);
 		sortedJobs.add(job1);
 		JobAssigner jobAssigner = new JobAssigner(unsortedJobs);
 		jobAssigner.sortJobs();
