75f9f9f4a1807b9c087051ff88122d70634b5ae9
ioanateju <ixt731@student.bham.ac.uk>
2018-03-10 16:42:32 +0000

    logging added and reader changed

--- Code/ServerSide/src/JobDecider/Reader.java
+++ Code/ServerSide/src/JobDecider/Reader.java
@@ -2,22 +2,24 @@ package JobDecider;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.concurrent.CancellationException;
+import org.apache.log4j.Logger;
 
 import DataObjects.Job;
 
-import com.sun.xml.internal.ws.api.Cancelable;
-
 public class Reader {
+
+	final static Logger logger = Logger.getLogger(Reader.class);
 
 	public static void main(String[] args) {
 
-		String itemFile = "/Users/RyanBolding/Documents/workspace/Test/items.csv";
-		String locationFile = "/Users/RyanBolding/Documents/workspace/Test/locations.csv";
+		String itemFile = "/home/tomas/Dropbox/Mokslai/RP/job files/items.csv";
+		String locationFile = "/home/tomas/Dropbox/Mokslai/RP/job files/locations.csv";
+		String jobFile = "/home/tomas/Dropbox/Mokslai/RP/job files/training_jobs.csv";
+		String cancellationFile = "/home/tomas/Dropbox/Mokslai/RP/job files/cancellations.csv";
 		BufferedReader br = null;
 		BufferedReader br1 = null;
 		String line = "";
@@ -25,58 +27,60 @@ public class Reader {
 		String cvsSplitBy = ",";
 		HashMap<String, Item> items = new HashMap<String, Item>();
 		ArrayList<Job> jobNumber = new ArrayList<Job>();
-
-		HashMap<Integer, Job> jobs = new HashMap<Integer, Job>();
-		String jobFile = "/Users/RyanBolding/Documents/workspace/Test/training_jobs.csv";
-		String cancellationFile = "/Users/RyanBolding/Documents/workspace/Test/cancellations.csv";
 		boolean cancel = false;
+
 		try {
 
 			br = new BufferedReader(new FileReader(itemFile));
 			br1 = new BufferedReader(new FileReader(locationFile));
-			while ((line = br.readLine()) != null
-					&& (line2 = br1.readLine()) != null) {
-
+			logger.debug("Reading items and locations csv files");
+			while ((line = br.readLine()) != null && (line2 = br1.readLine()) != null) {
 				// use comma as separator
 				String[] item = line.split(cvsSplitBy);
 				String[] locationArray = line2.split(cvsSplitBy);
 
-				items.put(
-						item[0],
-						new Item(Float.parseFloat(item[1]), Float
-								.parseFloat(item[2]), Integer
-								.parseInt(locationArray[0]), Integer
-								.parseInt(locationArray[1])));
-
-				// System.out.println(" [name= " + item[0] + " , reward=" +
-				// item[1] + "weight= " + item[2] + "]");
+				items.put(item[0], new Item(Float.parseFloat(item[1]), Float.parseFloat(item[2]),
+						Integer.parseInt(locationArray[0]), Integer.parseInt(locationArray[1])));
 
 			}
 
+			br.close();
+			br1.close();
 			br = new BufferedReader(new FileReader(jobFile));
 			br1 = new BufferedReader(new FileReader(cancellationFile));
-			while ((line = br.readLine()) != null
-					&& (line2 = br1.readLine()) != null) {
+			logger.debug("Reading training_jobs and cancellations csv files");
+			
+			while ((line = br.readLine()) != null && (line2 = br1.readLine()) != null) {
 				String[] job = line.split(cvsSplitBy);
 				int jobID = Integer.parseInt(job[0]);
 				String[] cancellation = line2.split(cvsSplitBy);
 				ArrayList<JobObject> itemList = new ArrayList<JobObject>();
+				
 				for (int i = 1; i < job.length - 1; i += 2) {
 					itemList.add(new JobObject(job[i], Integer.parseInt(job[i + 1])));
 				}
-				if(cancellation[1].equals("1")) cancel = true; 
-				jobNumber.add(new Job(jobID, itemList,items,cancel));
+				if (cancellation[1].equals("1"))
+					cancel = true;
 				
+				jobNumber.add(new Job(jobID, itemList, items, cancel));
 			}
-			System.out.println(jobNumber.get(3).getCancellation());
-			System.out.println(jobNumber.get(3).getID());
-			System.out.println(jobNumber.get(3).getJob().get(3).getID());
-			System.out.println(jobNumber.get(3).getJob().get(3).getQuantity());
-	
-			System.out.println(jobNumber.get(11).getJobReward());
 			
+			br.close();
+			br1.close();
+			
+//			int jobIndexToTest = 3;
+//			logger.debug("Is job cancelled? " + jobNumber.get(jobIndexToTest).getCancellation());
+//			logger.debug("Job ID: " + jobNumber.get(jobIndexToTest).getID());
+//			logger.debug("Is file cancelled?" + jobNumber.get(jobIndexToTest).getCancellation());
+//			logger.debug("One of the items that is in the job ID: " + jobNumber.get(jobIndexToTest).getJob().get(3).getID());
+//			logger.debug("The quantity of that item in the job: " + jobNumber.get(jobIndexToTest).getJob().get(3).getQuantity());
+//			logger.debug("The total job reward:  " + jobNumber.get(jobIndexToTest).getJobReward());
+//			logger.debug("The total job weight:  " + jobNumber.get(jobIndexToTest).getJobWeight());
+
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+			logger.error("One or more files are not found");
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
