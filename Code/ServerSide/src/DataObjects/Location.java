7804c52dd8a773f9c903ce9351c6873ac0c9b9b2
Henry Fung <hlf764@cs.bham.ac.uk>
2018-03-11 15:27:56 +0000

    Added methods getX() and getY().

--- Code/ServerSide/src/DataObjects/Location.java
+++ Code/ServerSide/src/DataObjects/Location.java
@@ -1,7 +1,18 @@
 package DataObjects;
 
 public class Location {
+	private Integer x, y;
+
 	Location(Integer x, Integer y){
-		
+		this.x = x;
+		this.y = y;
+	}
+
+	public Integer getX() {
+		return this.x;
+	}
+
+	public Integer getY() {
+		return this.y;
 	}
 }
