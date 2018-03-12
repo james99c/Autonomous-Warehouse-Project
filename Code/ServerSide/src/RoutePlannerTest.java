0260c97ad3543c573906f83457da46a07d945c0b
Henry Fung <hlf764@cs.bham.ac.uk>
2018-03-12 16:23:57 +0000

    Added compareList() function.

--- Code/ServerSide/src/RoutePlannerTest.java
+++ Code/ServerSide/src/RoutePlannerTest.java
@@ -1,4 +1,3 @@
-
 import DataObjects.*;
 
 import java.util.ArrayList;
@@ -7,14 +6,6 @@ import java.util.Arrays;
 import org.junit.*;
 
 public class RoutePlannerTest {
-
-	// RoutePlanner.main(RoutePlanner.java:50) 0 [main] DEBUG RoutePlanner - 0 : 1
-	// RoutePlanner.main(RoutePlanner.java:50) 2 [main] DEBUG RoutePlanner - 0 : 2
-	// RoutePlanner.main(RoutePlanner.java:50) 2 [main] DEBUG RoutePlanner - 1 : 2
-	// RoutePlanner.main(RoutePlanner.java:50) 2 [main] DEBUG RoutePlanner - 1 : 3
-	// RoutePlanner.main(RoutePlanner.java:50) 2 [main] DEBUG RoutePlanner - 1 : 4
-	// RoutePlanner.main(RoutePlanner.java:50) 2 [main] DEBUG RoutePlanner - 2 : 4
-	// RoutePlanner.main(RoutePlanner.java:50) 3 [main] DEBUG RoutePlanner - 3 : 4
 
 	// Map = no obstables
 	@Test
@@ -29,15 +20,15 @@ public class RoutePlannerTest {
 		ArrayList<GridPoint> desired = new ArrayList<GridPoint>();
 		desired.add(new GridPoint(0, 0, false));
 		desired.add(new GridPoint(0, 1, false));
-
 		desired.add(new GridPoint(0, 2, false));
 		desired.add(new GridPoint(0, 3, false));
 		desired.add(new GridPoint(0, 4, false));
 		desired.add(new GridPoint(1, 4, false));
 		desired.add(new GridPoint(2, 4, false));
 		desired.add(new GridPoint(3, 4, false));
+
 		ArrayList<GridPoint> result = planner.findRoute(start, end);
-		Assert.assertEquals(desired, result);
+		Assert.assert(compareList(desired, result));
 	}
 
 	@Test
@@ -51,15 +42,16 @@ public class RoutePlannerTest {
 		Location start = new Location(0, 0);
 		Location end = new Location(3, 4);
 		ArrayList<GridPoint> desired = new ArrayList<GridPoint>();
-		desired.add(new GridPoint(0, 1, true));
-		desired.add(new GridPoint(0, 2, true));
-		desired.add(new GridPoint(1, 2, true));
-		desired.add(new GridPoint(1, 3, true));
-		desired.add(new GridPoint(1, 4, true));
-		desired.add(new GridPoint(2, 4, true));
-		desired.add(new GridPoint(3, 4, true));
+		desired.add(new GridPoint(0, 1, false));
+		desired.add(new GridPoint(0, 2, false));
+		desired.add(new GridPoint(1, 2, false));
+		desired.add(new GridPoint(1, 3, false));
+		desired.add(new GridPoint(1, 4, false));
+		desired.add(new GridPoint(2, 4, false));
+		desired.add(new GridPoint(3, 4, false));
+
 		ArrayList<GridPoint> result = planner.findRoute(start, end);
-		Assert.assertEquals(desired, result);
+		Assert.assert(compareList(desired, result));
 	}
 
 	@Test
@@ -67,4 +59,18 @@ public class RoutePlannerTest {
 
 	}
 
+	private boolean compareList(ArrayList<GridPoint> a, ArrayList<GridPoint> b) {
+		if (a.size() != b.size()) return false;
+		int size = a.size();
+
+		for (int i = 0; i < size; i++) {
+			GridPoint p1 = a.get(i);
+			GridPoint p2 = b.get(i);
+
+			if (p1.getX() != p2.getX() || p1.getY() != p2.getY())
+				return false;
+		}
+
+		return true;
+	}
 }
