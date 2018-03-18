46022fb2d2a4f06d9f5359c2960c9b9b21d8d24b
jdw725 <JDW725@student.bham.ac.uk>
2018-03-18 15:22:01 +0000

    Added to interface, changed method signatures to match route planner.

--- Code/ServerSide/src/Interfaces/JobAssignerInterface.java
+++ Code/ServerSide/src/Interfaces/JobAssignerInterface.java
@@ -10,5 +10,7 @@ public interface JobAssignerInterface {
 
 	//public void sortJobs();
 	//public HashMap<String, ArrayList<Job>> outputJobs(ArrayList<Job> sortedJobs);
-	public ArrayList<Location> assignJob(Location currentLocation);
+	public ArrayList<Location> assignJob(Location currentLocation, String robotName);
+	public Job getBestJob(Location currentLocation);
+	public ArrayList<Location> getRoute(Location _startLocation, Job job, String robotName);
 }
